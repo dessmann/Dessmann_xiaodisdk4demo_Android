7c7555f4312626eac37ea8328c17ec5b72868b5f
净琉璃 <yiyeshu1015@vip.qq.com>
2018-10-17 17:54:02 +0800

    添加设备闪退问题修复

--- app/src/main/java/com/yunds/xiaodisdk4demo/MainActivity.java
+++ app/src/main/java/com/yunds/xiaodisdk4demo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.yunds.xiaodisdk4demo;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.blankj.utilcode.util.LogUtils;
 import com.bluetoothle.core.BLELoader;
 import com.dsm.xiaodisdk4.base.Xiaodi4ApiRequestFactory;
 import com.dsm.xiaodisdk4.base.XiaodiBleFactory;
@@ -17,27 +21,21 @@ import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BLELoader.init();
+        dialog = new ProgressDialog(this);
         /*demoCall();*/
     }
 
     private void demoCall() {
-        Xiaodi4ApiRequestFactory.Companion.getInstance().getRegisterLockRequest("", "", false, new OnAddDeviceListener() {
-            @Override
-            public void addSuccess(@Nullable String lockMac, @Nullable String rootKey, @Nullable String lockSn, @Nullable String lockChannelPassword, @Nullable String lockSoftwareVersion, @Nullable String lockSeid) {
-
-            }
-
-            @Override
-            public void addFailure(@Nullable Integer errCode) {
-
-            }
-        }).request();
         Xiaodi4ApiRequestFactory.Companion.getInstance().getOpenLockRequest("", "", "", false, "", new OnOpenListener() {
             @Override
             public void openSuccess(@NotNull Object obj) {
@@ -76,5 +74,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void onRegisterDevice(View view) {
+        dialog.show();
+        Xiaodi4ApiRequestFactory.Companion.getInstance().getRegisterLockRequest("FE:0A:A3:D2:97:8E", "18668165281", false, new OnAddDeviceListener() {
+            @Override
+            public void addSuccess(@Nullable String lockMac, @Nullable String rootKey, @Nullable String lockSn, @Nullable String lockChannelPassword, @Nullable String lockSoftwareVersion, @Nullable String lockSeid) {
+                dialog.dismiss();
+                LogUtils.i(TAG, "lockMac=" + lockMac + ",rootKey=" + rootKey + ",lockSn=" + lockSn + ",lockChannelPassword=" + lockChannelPassword + ",lockSoftwareVersion=" + lockSoftwareVersion + ",lockSeid=" + lockSeid);
+            }
+
+            @Override
+            public void addFailure(@Nullable Integer errCode) {
+                dialog.dismiss();
+                LogUtils.e(TAG, "errorCode=" + errCode);
+            }
+        }).request();
     }
 }
